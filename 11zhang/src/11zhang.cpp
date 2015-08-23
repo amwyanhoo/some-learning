0d9dc5ffc7d76c4a9e861640de090281d9009f4d
amwyanhoo <amwyanhoo@gmail.com>
2015-08-23 19:58:05 +0800

    C++容器

---
+++ 11zhang/src/11zhang.cpp
@@ -0,0 +1,94 @@
+//#include <iostream>
+//#include <list>
+//#include <vector>
+//#include <numeric>
+//#include <stack>
+//using namespace std;
+//
+//int main() {
+////vector<int> v;
+////	int i;
+////	for(i=0;i<10;i++){
+////		v.push_back(i);
+////	}
+////	for(int i=0;i<10;i++){
+////		cout<<v[i]<<" ";
+////	}
+////	cout<<endl;
+////	v.erase(v.begin()+2);
+////	vector<int>::iterator it;
+////
+////
+////	for(it=v.begin();it!=v.end();it++){
+////		cout<<*it<<" ";
+////	}
+////	cout<<endl;
+////	v.pop_back();
+////	cout<<v.back()<<endl;
+////	cout<<v.at(2)<<endl;
+////	v.assign(10,2);
+////vector<int>::iterator it;
+////	vector<int>::iterator it1;
+////	it=v.begin();
+////	it1=v.end();
+////
+////	v.assign(it,it1);
+////	cout<<v.capacity()<<endl;
+////	v.clear();
+////	cout<<v.empty()<<endl;
+////	for(it=v.begin();it!=v.end();it++){
+////		cout<<*it<<" ";
+////	}
+////	cout<<endl;
+////	cout<<accumulate(v.begin(),v.end(),0)<<endl;
+////
+////	vector<char> alphaVector;
+////	for(int i=0;i<10;i++){
+////		alphaVector.push_back(i+65);
+////	}
+////	int size=alphaVector.size();
+////
+////
+////	vector<char>::iterator it1;
+////	vector<char>::iterator it2;
+////
+////	for(it2=alphaVector.begin();it2!=alphaVector.end();it2++){
+////				cout<<*it2<<" ";
+////	}
+////				cout<<endl;
+////
+////	for(int i=0;i<size;i++){
+////		it1=alphaVector.begin();
+////		alphaVector.erase(it1);
+////		for(it2=alphaVector.begin();it2!=alphaVector.end();it2++){
+////			cout<<*it2<<" ";
+////		}
+////			cout<<endl;
+////	}
+////
+////	list<int> l;
+////	for(int i=0;i<10;i++){
+////		l.push_back(i);
+////	}
+////
+////	list<int>::iterator it1;
+////	l.insert(l.begin(),10);
+////	list<int>::iterator it;
+////	for(it=l.begin();it!=l.end();it++){
+////		cout<<*it<<" ";
+////	}
+//
+//	stack<int> s;
+//	s.push(1);
+//	s.push(2);
+//	s.push(3);
+//	s.push(8);
+//	while(!s.empty()){
+//		cout<<s.top()<<" ";
+//		s.pop();
+//	}
+//	cout<<endl;
+//	return 0;
+//}
+
+
